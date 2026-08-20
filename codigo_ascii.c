ff81fe1f2614aa18d589b8b2ccb1b63005a04115
Melissa Santos <melissilva123@gmail.com>
2026-08-20 16:22:53 -0300

    Conversão de um caractere digitado em octal, decimal e hexadecimal

---
+++ codigo_ascii.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    char caractere;
+    printf("\n Digite um caractere: ");
+    scanf("%c", &caractere);
+    printf("\n %o", caractere);
+    printf("\n %d", caractere);
+    printf("\n %x", caractere);
+    return 0;
+}
